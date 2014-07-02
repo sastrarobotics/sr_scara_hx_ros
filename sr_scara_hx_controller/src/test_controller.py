f36fea681001acde94b590188c9d8e4b68ff8f9e
achu <achuwilson@gmail.com>
2014-07-03 00:24:46 +0530

    joint_state_publisher  moveit hardware interface ok

--- sr_scara_hx_controller/src/test_controller.py
+++ sr_scara_hx_controller/src/test_controller.py
@@ -1,15 +1,20 @@
 import rospy
+import herkulex
+import math
 from sensor_msgs.msg import JointState
 
 num_joints = 4
+herkulex.connect("/dev/ttyUSB0",115200)
+herkulex.torque_off(1)
+herkulex.torque_off(2)
 
 
 
 def joint_state_publisher():
-    pub = rospy.Publisher('sr_joint_states', JointState)
+    pub = rospy.Publisher('sr_joint_states', JointState,queue_size = 5)
     rospy.init_node('sr_jnt_stt')
     
-    r = rospy.Rate(10)
+    r = rospy.Rate(300)
     while not rospy.is_shutdown():
         msg = JointState()
         msg.header.stamp = rospy.Time.now()
@@ -17,8 +22,8 @@ def joint_state_publisher():
         msg.velocity = num_joints * [0.0]
         #msg.effort = num_joints * [0.0]
         msg.name =  ['joint1', 'joint2', 'joint3', 'gripper_con']
-        msg.position[0] = 1.0
-        msg.position[1] =0.2
+        msg.position[0] = math.radians(herkulex.get_servo_angle(1))
+        msg.position[1] = math.radians(herkulex.get_servo_angle(2))
         msg.position[2] =0.3
         msg.position[3] =0.4
         pub.publish(msg)
